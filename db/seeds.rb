41b463616fef19e1ce8a034cf5205bd2e6dabced
Victor <victormza@gmail.com>
2014-11-03 23:23:08 -0200

    Se agrego seed de departamentos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# ruby encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+lista_departamentos = [
+	"Capital",
+	"Godoy Cruz",
+	"Gral. Alvear",
+	"Guaymallen",
+	"Junín",
+	"La Paz",
+	"Las Heras",
+	"Lavalle",
+	"Luján",
+	"Maipú",
+	"Malargüe",
+	"Rivadavia",
+	"San Carlos",
+	"San Martín",
+	"San Rafael",
+	"Santa Rosa",
+	"Tunuyán",
+	"Tupungato"
+]
+
+lista_departamentos.each do |departamento|
+	DptoMendoza.create(nombre: departamento)
+end
