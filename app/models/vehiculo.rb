cbc293649c9fd2e254a7ee376fffb35ce4598110
Victor <victormza@gmail.com>
2014-11-13 04:55:40 -0300

    Pequeños cambios

--- app/models/vehiculo.rb
+++ app/models/vehiculo.rb
@@ -9,7 +9,17 @@ class Vehiculo < ActiveRecord::Base
   accepts_nested_attributes_for :fotos, :reject_if => :all_blank, :allow_destroy => true
 
   validates_presence_of   :dpto_mendoza_id
-  validates_presence_of   :titulo
+  # validates_presence_of   :titulo
+  validates_presence_of   :marca
+  validates_presence_of   :modeloCustom
+  validates_presence_of   :anio
+  validates_presence_of   :precio
+  validates_presence_of   :combustible
+  validates_presence_of   :kilometraje
+  validates_presence_of   :condicion
+  
+  # HACER: Ver validaciones de imagenes con paperclip, y realizar validaciones que faltan, como la regex para el modelo del vehículo
+  # HACER: Acordar cuales son las validaciones obligatorias en el primer paso, y realizar seeds para poblar la db
 
   has_attached_file :foto_ppal, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.jpg"
   validates_attachment_content_type :foto_ppal, :content_type => /\Aimage\/.*\Z/
